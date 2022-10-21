e7f22a25dde7122e1008ba91357fcde0a9c70f2c
Luvie Rallos <luvierallos@gmail.com>
2022-10-21 10:28:41 +0800

    complete form layout

--- src/todoform.js
+++ src/todoform.js
@@ -4,43 +4,51 @@ export default function todoForm(){
     const content = document.getElementById('content');
 
     const form = document.createElement('form');
-    form.setAttribute('id','form')
+    form.setAttribute('id','form');
 
     const title = document.createElement('input');
     title.setAttribute('id', 'title');
     title.setAttribute('type', 'text');
-    const titleLabel = document.createElement('label')
-    titleLabel.innerText = 'Title:'
+    const titleLabel = document.createElement('label');
+    titleLabel.innerText = 'Title:';
     titleLabel.setAttribute('for', 'title');
 
 
-    const description = document.createElement('input');
+    const description = document.createElement('textarea');
     description.setAttribute('id', 'description');
     description.setAttribute('type', 'text');
-    const descriptionLabel = document.createElement('label')
-    descriptionLabel.innerText = 'Description:'
+    const descriptionLabel = document.createElement('label');
+    descriptionLabel.innerText = 'Description:';
     descriptionLabel.setAttribute('for', 'description');
 
 
     const dueDate = document.createElement('input');
     dueDate.setAttribute('id', 'dueDate');
     dueDate.setAttribute('type', 'date');
-    const dueDateLabel = document.createElement('label')
-    dueDateLabel.innerText = 'Due Date:'
+    const dueDateLabel = document.createElement('label');
+    dueDateLabel.innerText = 'Due Date:';
     dueDateLabel.setAttribute('for', 'dueDate');
 
-    if (form.parentElement === document.body){
-        document.body.removeChild(form)
-    } 
+    const buttons = document.createElement('div');
 
+    const saveButton = document.createElement('button');
+    saveButton.setAttribute('type', 'button');
+    saveButton.textContent = 'Save';
 
+    const closeButton = document.createElement('button');
+    closeButton.setAttribute('type', 'button');
+    closeButton.textContent = 'X';
+
+    buttons.appendChild(closeButton);
+    buttons.appendChild(saveButton);
     content.appendChild(form);
-    form.appendChild(titleLabel)
+    form.appendChild(titleLabel);
     form.appendChild(title);
-    form.appendChild(descriptionLabel)
+    form.appendChild(descriptionLabel);
     form.appendChild(description);
-    form.appendChild(dueDateLabel)
+    form.appendChild(dueDateLabel);
     form.appendChild(dueDate);
+    form.appendChild(buttons);
 
     
 
