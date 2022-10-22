bf1d2ad55e49b122d5fbeeab43c5dd4163c11677
Luvie Rallos <luvierallos@gmail.com>
2022-10-22 11:34:30 +0800

    add done button

--- src/todoDOM.js
+++ src/todoDOM.js
@@ -9,7 +9,25 @@ export default function createCards(todos){
         element.setAttribute('id', 'card' + i);
         element.className = "cards";
         element.innerHTML = `${todos[i].title} this nuts`
+
+        let buttons = document.createElement('div');
+        buttons.className = "buttons";
+
+        let button = document.createElement('button');
+        button.setAttribute('id','remove-button'+i);
+        button.textContent = 'Done';
+        button.className = "card-button";
+        button.onclick = function(){
+            remove(button.getAttribute('id').charAt(button.getAttribute('id').length - 1)) ;
+        }
+
+        element.appendChild(button)
         board.appendChild(element)
     }
 
+}
+
+function remove(index){
+    myLibrary.splice(index,1);
+    setupShelf();
 }
